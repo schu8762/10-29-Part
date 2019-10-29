59f2703f582e5ec4d812ac2f069bf328c495e022
schu8762 <J.Schuller@ou.edu>
2019-10-29 10:53:59 -0500

    first

--- 10-29 Part/Program.cs
+++ 10-29 Part/Program.cs
@@ -10,14 +10,14 @@ namespace _10_29_Part
     {
         static void Main(string[] args)
         {
+            Rectangle rect1 = new Rectangle();
+            rect1.Length = 5;
+            rect1.Width = 10;
 
-
-
-
-
-
-
-
+            //p1 is the defined name for perimeter
+            double p1 = rect1.CalculatePerimeter();
+                                          
+            Console.WriteLine($"Rectangle 1 with {rect1.Length} length and {rect1.Width} width has a perimiter of {p1} and an area of {rect1.CalculateArea()}");
 
 
             Console.ReadKey();
---
+++ 10-29 Part/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_29_Part
+{
+    class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+
+        //CalculateArea(length : int ) : Double if there were parameters
+        public double CalculateArea()
+        {
+            double area = Length * Width;
+            return area;
+        }
+
+                    
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+
+
+
+
+
+    }
+}
